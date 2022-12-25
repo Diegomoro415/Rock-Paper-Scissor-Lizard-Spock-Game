7b4766dae53f303bc33861ce8369915057db0d67
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-25 14:14:56 +0000

    initial script commti - set variables

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+/*jshint esversion: 6 */
+//variable score
+let scoreUser = 0;
+let scorePc = 0;
+//variable Players
+let player1 = "";
+let computer = "";
+//aplications
+let displayPlayer1 = document.getElementById("player1");
+let displayComputer = document.getElementById("computer");
+let score = document.getElementById("score-board");
+let play = document.getElementById("play");
+let winner = document.getElementById("winner");
+let loser = document.getElementById("loser");
+
