ea864cde404e511ec138a96bd2021d8ce4c94079
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-26 14:19:51 +0000

    style final-screen class

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 /*jshint esversion: 6 */
 //variable score
-let scoreUser = 0;
-let scorePc = 0;
+var scoreUser = 0;
+var scorePc = 0;
 //variable Players
-let player1 = "";
-let computer = "";
+var player1 = "";
+var computer = "";
 //aplications
-let displayPlayer1 = document.getElementById("player1");
-let displayComputer = document.getElementById("computer");
-let score = document.getElementById("score-board");
-let play = document.getElementById("play");
-let winner = document.getElementById("winner");
-let loser = document.getElementById("loser");
-
+const displayPlayer1 = document.getElementById("player1");
+const displayComputer = document.getElementById("computer");
+const scoreBoard = document.getElementById("score-board");
+const play = document.getElementById("playing");
+const winner = document.getElementById("winner");
+const loser = document.getElementById("loser");
