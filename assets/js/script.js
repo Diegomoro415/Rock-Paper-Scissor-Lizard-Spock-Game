16b90170630f174134e425799b9f7269af8b3d82
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-26 16:52:03 +0000

    add function checkResult, if statement with the game rule and add if statement to the score

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,8 @@ const loser = document.getElementById("loser");
 
 // Calls startGame and playPc function after user's click
 playing.addEventListener("click", () => {
-    startGame()
-    playPc()
+    startGame();
+    playPc();
 })
 /** 
  * Gets the values of user's choice
@@ -24,9 +24,9 @@ playing.addEventListener("click", () => {
 function startGame() {
     //collect value from input name=play that has been selected
     //displays the value of player1 concatenated with the image's address
-    player1 = document.querySelector('input[name="play"]:checked').value
-    displayPlayer1.innerHTML = "<img src='assets/images/" + player1 + ".png'>"
-    displayComputer.innerHTML = ""
+    player1 = document.querySelector('input[name="play"]:checked').value;
+    displayPlayer1.innerHTML = "<img src='assets/images/" + player1 + ".png'>";
+    displayComputer.innerHTML = "";
 }
 /**
  * Displays a random value for computer
@@ -37,7 +37,75 @@ function playPc() {
     //gets a random choice for computar
     let num = Math.floor(Math.random() * 5);
     //displays the value of computer concatenated with the image's address
-    computer = opt[num]
-    displayComputer.innerHTML = "<img src='assets/images/" + computer + ".png'>"
-    checkResult()
+    computer = opt[num];
+    displayComputer.innerHTML = "<img src='assets/images/" + computer + ".png'>";
+    checkResult();
+}
+/**
+ * Game Rules
+ */
+function checkResult() {
+
+    play.disabled = true
+
+    let win = "0";
+     //checks player 1 choice and computer choice to determinate who wins
+     // 1 win, -1 lose and 0 game is a tie
+    if (player1 == computer) {
+    } else if (player1 == 'rock') {
+        win = computer == 'scissor' ? 1 : -1
+    } else if (player1 == 'paper') {
+        win = computer == 'rock' ? 1 : -1
+    } else if (player1 == 'scissor') {
+        win = computer == 'paper' ? 1 : -1
+    } else if (player1 == 'lizard') {
+        win = computer == 'paper' ? 1 : -1
+    } else if (player1 == 'spock') {
+        win = computer == 'scissor' ? 1 : -1
+    }
+    
+    if (player1 == computer) {
+    } else if (player1 == 'rock') {
+        win = computer == 'lizard' ? 1 : -1
+    } else if (player1 == 'paper') {
+        win = computer == 'spock' ? 1 : -1
+    } else if (player1 == 'scissor') {
+        win = computer == 'lizard' ? 1 : -1
+    } else if (player1 == 'lizard') {
+        win = computer == 'spock' ? 1 : -1
+    } else if (player1 == 'spock') {
+        win = computer == 'rock' ? 1 : -1
+    }
+
+    // if win bigger than 0 (a tie game) increase by 1 scoreUser
+    // if win smaller than 0 (a tie game) increase by 1 scorePc
+    if(win == 0){
+                                                                                                                 
+    } else if (win > 0) {
+        scoreUser = scoreUser + 1
+        
+    } else {
+        scorePc = scorePc + 1
+    }
+    
+    //displays score
+    scoreBoard.innerHTML = scoreUser +  ":" + scorePc
+    
+    //defines tha game is over after 5 wins
+    //user reaches 5 wins pop up winner final-screen
+    if (scoreUser >= 5){
+        winner.classList.remove('none')
+        winner.classList.add('center')
+    }
+    
+    //computer reaches 5 wins pop up loser final-screen
+    if (scorePc >= 5){
+        loser.classList.remove('none')
+        loser.classList.add('center')
+    }
+    
+    //set time 1 second to the next try
+    setTimeout(()=>{
+        play.disabled = false
+    },1000)
 }
