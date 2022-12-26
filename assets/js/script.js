a803ae996872f775b61ff69cafa637370adbdeee
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-26 14:47:36 +0000

    add function startGame

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,13 @@ playing.addEventListener("click", () => {
     startGame()
     playPc()
 })
+/** 
+ * Gets the values of user's choice
+ */
+function startGame() {
+    //collect value from input name=play that has been selected
+    //displays the value of player1 concatenated with the image's address
+    player1 = document.querySelector('input[name="play"]:checked').value
+    displayPlayer1.innerHTML = "<img src='assets/images/" + player1 + ".png'>"
+    displayComputer.innerHTML = ""
+}
