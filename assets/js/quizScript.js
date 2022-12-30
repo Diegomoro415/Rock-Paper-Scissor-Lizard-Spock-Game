26af6af9795146905930e44fdf90e2cbc5e4b1eb
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-30 19:08:33 +0000

    add initial style and initial variables to quizScript

---
+++ assets/js/quizScript.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6*/
+var question = document.getElementById("question");
+var answers = document.getElementById("answers");
+var numQuestion = document.getElementById("num-question");
+var quizContainer = document.getElementById("quiz-container");
+var endQuiz = document.getElementById("end-quiz");
+var endQuizScreen = document.getElementById("end-quiz-screen");
+var quizButton = document.getElementById("btn-end-screen");
+
+let 
+
+var shuffedQuestions;
