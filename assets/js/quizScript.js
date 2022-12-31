d846de90d5ad6aa2694c1548e2d8bc676404f3bf
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-31 14:20:36 +0000

    add loadQuestions function to quiz.js

--- assets/js/quizScript.js
+++ assets/js/quizScript.js
@@ -7,7 +7,38 @@ var endQuiz = document.getElementById("end-quiz");
 var endQuizScreen = document.getElementById("end-quiz-screen");
 var quizButton = document.getElementById("btn-end-screen");
 
-import questions from "./questions.js";
-
-
 var shuffedQuestions;
+
+import questions from "./questions.js";
+//Displyas number of current question and number of correct answers
+let currentQuestion = 0;
+let correctAnswers = 0;
+
+/**
+ * Get the current question number  and shows how many question to the end
+ * everytime load question  add by 1 current question number
+ */
+function loadQuestions() {
+    numQuestion.innertHTML = `${currentQuestion +1} / ${questions.length}`;
+    const item = questions[currentQuestion];
+    answers.innerHTML = "";
+    question.innerHTML = item.question;
+// create a forEach for answer and create a new div
+    item.answers.forEach((answer) => {
+        const div = document.createElement("div");
+    //create button for answers, colletct data for correct answer and display answers options
+        div.innerHTML = `
+        <button class="answer" data-correct="${answer.correct}">
+          ${answer.option}
+        </button>
+        `;
+   // Append div to answers
+        answers.appendChild(div);
+      });
+      // Querry selector to all these buttons,  forEach to item to assign click events to each button
+      document.querySelectorAll(".answer").forEach((item) => {
+        item.addEventListener("click", nextQuestion);
+      });
+    }
+    loadQuestions();
+
