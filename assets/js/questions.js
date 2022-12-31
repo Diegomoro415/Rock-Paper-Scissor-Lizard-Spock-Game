92f1135006c2aa23b46441747740eed1eed3e7b0
Diegomoro415 <diego_moro415@hotmail.com>
2022-12-31 13:18:18 +0000

    add question sript file, add all question to quiz into the file

---
+++ assets/js/questions.js
@@ -0,0 +1,257 @@
+export default [
+    {
+        question: "What country has the highest life expectancy?",
+        answers: [
+            {
+                option: "Japan", correct: false
+            },
+            {
+                option: "Nepal", correct: false
+            },
+            {
+                option: "Hong Kong", correct: true
+            },
+            {
+                option: "Singapure", correct: false
+            },
+        ]
+    },
+    {
+        question: "What artist has the most streams on Spotify?",
+        answers: [
+            {
+                option: "Drake", correct: true
+            },
+            {
+                option: "Taylor Swift", correct: false
+            },
+            {
+                option: "Ed Sheeran", correct: false
+            },
+            {
+                option: "Beatles", correct: false
+            },
+        ],
+    },
+    {
+        question: "What country has the most islands?",
+        answers: [
+            {
+                option: "Greece", correct: false
+            },
+            {
+                option: "Sweden", correct: true
+            },
+            {
+                option: "Thailand", correct: false
+            },
+            {
+                option: "Indonesia", correct: false
+            },
+        ],
+    },
+    {
+        question: "How many hearts does an octopus have?",
+        answers: [
+            {
+                option: "8", correct: false
+            },
+            {
+                option: "3", correct: true
+            },
+            {
+                option: "4", correct: false
+            },
+            {
+                option: "7", correct: false
+            },
+        ],
+    },
+    {
+        question: "On what continent would you find the world's largest desert?",
+        answers: [
+            {
+                option: "Africa", correct: false
+            },
+            {
+                option: "Asia", correct: false
+            },
+            {
+                option: "Oceania", correct: false
+            },
+            {
+                option: "Antartica", correct: true
+            },
+        ],
+    },
+    {
+        question: "What is the Capital of Australia?",
+        answers: [
+            {
+                option: "Melbourne", correct: false
+            },
+            {
+                option: "Brisbane", correct: false
+            },
+            {
+                option: "Sydney", correct: false
+            },
+            {
+                option: "Canberra", correct: true
+            },
+        ],
+    },
+    {
+        question: "What country drinks the most coffee per capita?",
+        answers: [
+            {
+                option: "China", correct: false
+            },
+            {
+                option: "U.S.A", correct: false
+            },
+            {
+                option: "Finland", correct: true
+            },
+            {
+                option: "England", correct: false
+            },
+        ],
+    },
+    {
+        question: "Where did sushi originate?",
+        answers: [
+            {
+                option: "Taiwan", correct: false
+            },
+            {
+                option: "China", correct: true
+            },
+            {
+                option: "South Korea", correct: false
+            },
+            {
+                option: "Japan", correct: false
+            },
+        ],
+    },
+    {
+        question: "What does FIFA stand for in English?",
+        answers: [
+            {
+                option: "Federation International Football Association", correct: false
+            },
+            {
+                option: "International Federation of Association Football", correct: true
+            },
+            {
+                option: "International Federation of Football Association", correct: false
+            },
+            {
+                option: "Football Internationl of Federation Association", correct: false
+            },
+        ],
+    },
+    {
+        question: "Who is the world's highest-paid athlete in 2022?",
+        answers: [
+            {
+                option: "Cristiano Ronaldo ", correct: false
+            },
+            {
+                option: "Lionel Messi", correct: true
+            },
+            {
+                option: "Connor McGregor", correct: false
+            },
+            {
+                option: "LeBron James", correct: false
+            },
+        ],
+    },
+    {
+        question: "What is the top longest river in the world?",
+        answers: [
+            {
+                option: "Nile", correct: true
+            },
+            {
+                option: "Amazon River", correct: false
+            },
+            {
+                option: "Congo River", correct: false
+            },
+            {
+                option: "Mississippi River", correct: false
+            },
+        ],
+    },
+    {
+        question: "What is the Safest city in the World in 2022?",
+        answers: [
+            {
+                option: "Singapore", correct: false
+            },
+            {
+                option: "Copenhagen", correct: false
+            },
+            {
+                option: "Genebra", correct: false
+            },
+            {
+                option: "Tokyo", correct: true
+            },
+        ],
+    },
+    {
+        question: "What is the most powerfull passport in the World?",
+        answers: [
+            {
+                option: "Germany", correct: false
+            },
+            {
+                option: "Luxembourg", correct: false
+            },
+            {
+                option: "United Arab Emirates", correct: true
+            },
+            {
+                option: "Sweden", correct: false
+            },
+        ],
+    },
+    {
+        question: "What is the most dangerous city in the world?",
+        answers: [
+            {
+                option: "Tijuana - Mexico", correct: true
+            },
+            {
+                option: "Caracas - Venezuela", correct: false
+            },
+            {
+                option: "San Pedro Sula - Honduras", correct: false
+            },
+            {
+                option: "San Salvador - El Salvador", correct: false
+            },
+        ],
+    },
+    {
+        question: "What is the lowest point on Earth?",
+        answers: [
+            {
+                option: "Bermuda Triangle", correct: false
+            },
+            {
+                option: "Mauricius Island", correct: false
+            },
+            {
+                option: "Dead Sea", correct: true
+            },
+            {
+                option: "Dead Valley", correct: false
+            },
+        ],
+    },
+]
